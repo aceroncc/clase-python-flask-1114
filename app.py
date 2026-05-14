adf331c275f92431408d27a502ddfa1bb637ec96
aceroncc <aceronc867@inemkennedy.edu.co>
2026-05-14 13:46:04 -0500

    hi

--- app.py
+++ app.py
@@ -1,26 +1,32 @@
-
- 
- from flask import Flask, render_template
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.route('/')
+def inicio():
+    # 1. Definimos las 3 variables (Punto 2 de tu tarea)
+    titulo_pagina = "Panel de Control"
+    nombre_usuario = "Sophia"
+    temas_vistos = ["Rutas", "Plantillas", "Variables"]
+   
+    # 2. Las pasamos al template (Punto 3 de tu tarea)
+    return render_template('index.html',
+                           titulo=titulo_pagina,
+                           usuario=nombre_usuario,
+                           temas=temas_vistos)
 
+@app.route('/index')
+def index():
+    return render_template('index.html')
 
-@app.route("/index1")
-def index1():
-    return render_template("index1.html")
-
-
-
-@app.route("/index2")
+@app.route('/index2')
 def index2():
-    return render_template("index2.html")
+    return render_template('index2.html')
 
-
-@app.route("/index3")
+@app.route('/index3')
 def index3():
-    return render_template("index3.html")
+    return render_template('index3.html')
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
+    # Ejecutamos el servidor en modo debug
     app.run(debug=True)
